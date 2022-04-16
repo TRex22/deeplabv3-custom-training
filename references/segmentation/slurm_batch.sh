08e40a8ac6d8b56b7969bda06f0ca02ac7ddca2c
trex22 <contact@jasonchalom.com>
2022-04-17 01:56:59 +0200

    Add in cluster paths :cop:

--- references/segmentation/slurm_batch.sh
+++ references/segmentation/slurm_batch.sh
@@ -6,5 +6,5 @@
 #SBATCH --nodes=1 
 #SBATCH --partition=biggpu
 
-# torchrun train.py --data-path /mnt/excelsior/data/coco/data_raw/ --device cuda --lr 0.02 --dataset coco -b 48 -j 28 --epochs 30 --model deeplabv3_resnet50 --aux-loss --weights-backbone ResNet101_Weights.IMAGENET1K_V1 --output-dir /data/models/vision/
-torchrun train.py --data-path /mnt/excelsior/data/coco/data_raw/ --device cuda --lr 0.02 --dataset coco -b 24 -j 28 --epochs 30 --model deeplabv3_resnet101 --aux-loss --weights-backbone ResNet101_Weights.IMAGENET1K_V1 --output-dir /data/models/vision/
+# torchrun train.py --data-path /home-mscluster/jchalom/data/coco/zips --device cuda --lr 0.02 --dataset coco -b 48 -j 28 --epochs 30 --model deeplabv3_resnet50 --aux-loss --weights-backbone ResNet101_Weights.IMAGENET1K_V1 --output-dir /home-mscluster/jchalom/trained_models/deeplabv3
+torchrun train.py --data-path /home-mscluster/jchalom/data/coco/zips --device cuda --lr 0.02 --dataset coco -b 24 -j 28 --epochs 30 --model deeplabv3_resnet101 --aux-loss --weights-backbone ResNet101_Weights.IMAGENET1K_V1 --output-dir /home-mscluster/jchalom/trained_models/deeplabv3

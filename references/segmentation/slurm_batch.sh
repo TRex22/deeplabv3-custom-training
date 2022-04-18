e88bafce781cb3a222844da6896fef9e20531df7
trex22 <contact@jasonchalom.com>
2022-04-18 22:21:26 +0200

    Add in retry code :cop:

--- references/segmentation/slurm_batch.sh
+++ references/segmentation/slurm_batch.sh
@@ -2,11 +2,12 @@
 #SBATCH --job-name=deeplabv3
 #SBATCH --output=/home-mscluster/jchalom/deeplabv3_result.txt
 #SBATCH --ntasks=1
-#SBATCH --time=2250:00
+#SBATCH --time=2550:00
 #SBATCH --nodes=1 
 #SBATCH --partition=biggpu
 
 # torchrun train.py --data-path /home-mscluster/jchalom/data/coco/zips --device cuda --lr 0.02 --dataset coco -b 48 -j 28 --epochs 30 --model deeplabv3_resnet50 --aux-loss --weights-backbone ResNet101_Weights.IMAGENET1K_V1 --output-dir /home-mscluster/jchalom/trained_models/deeplabv3
-torchrun train.py --data-path /home-mscluster/jchalom/data/coco/zips --device cuda --lr 0.02 --dataset coco -b 24 -j 28 --epochs 30 --model deeplabv3_resnet101 --aux-loss --weights-backbone ResNet101_Weights.IMAGENET1K_V1 --output-dir /home-mscluster/jchalom/trained_models/deeplabv3
+# torchrun train.py --data-path /home-mscluster/jchalom/data/coco/zips --device cuda --lr 0.02 --dataset coco -b 24 -j 28 --epochs 30 --model deeplabv3_resnet101 --aux-loss --weights-backbone ResNet101_Weights.IMAGENET1K_V1 --output-dir /home-mscluster/jchalom/trained_models/deeplabv3
 
-# TODO: Continue here
+# Continue - set as needed
+torchrun train.py --data-path /home-mscluster/jchalom/data/coco/zips --device cuda --lr 0.02 --dataset coco -b 24 -j 28 --epochs 30 --model deeplabv3_resnet101 --aux-loss --weights-backbone ResNet101_Weights.IMAGENET1K_V1 --output-dir /home-mscluster/jchalom/trained_models/deeplabv3 --start-epoch 12 --resume /home-mscluster/jchalom/trained_models/deeplabv3/checkpoint.pth

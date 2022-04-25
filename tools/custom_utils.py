31ab5f34025d0d86ce98112178000ad59afcdd9c
trex22 <contact@jasonchalom.com>
2022-04-25 19:04:37 +0200

    Convert inputs :cop:

--- tools/custom_utils.py
+++ tools/custom_utils.py
@@ -62,7 +62,7 @@ def collate_cityscapes(batch):
     images.append(transforms.ToTensor()(batch[i][0]))
     targets.append(transforms.ToTensor()(batch[i][1]))
 
-  return torch.Tensor(images), torch.Tensor(targets)
+  return torch.Tensor(np.array(images)), torch.Tensor(np.array(targets))
 
 def load_dataset(config, root, image_set, category_list=None, batch_size=1, sample=False):
   if config["dataset"] == "COCO16" or config["dataset"] == "COCO21":

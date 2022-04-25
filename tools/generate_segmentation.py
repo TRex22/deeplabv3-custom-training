153cd99e90b390bcc5fffd304472cde1be904fc3
trex22 <contact@jasonchalom.com>
2022-04-26 01:09:59 +0200

    Begin to make segmentation maps :cop:

--- tools/generate_segmentation.py
+++ tools/generate_segmentation.py
@@ -5,6 +5,57 @@ from PIL import Image
 
 import custom_utils
 
+# https://github.com/mcordts/cityscapesScripts/blob/master/cityscapesscripts/helpers/labels.py
+cityscapes_colours = [
+  [  0,  0,  0],
+  [  0,  0,  0],
+  [  0,  0,  0],
+  [  0,  0,  0],
+  [  0,  0,  0],
+  [  0,  0,  0],
+  [111, 74,  0],
+  [ 81,  0, 81],
+  [128, 64,128],
+  [244, 35,232],
+  [250,170,160],
+  [230,150,140],
+  [ 70, 70, 70],
+  [102,102,156],
+  [190,153,153],
+  [180,165,180],
+  [150,100,100],
+  [150,120, 90],
+  [153,153,153],
+  [153,153,153],
+  [250,170, 30],
+  [220,220,  0],
+  [107,142, 35],
+  [152,251,152],
+  [ 70,130,180],
+  [220, 20, 60],
+  [255,  0,  0],
+  [  0,  0,142],
+  [  0,  0, 70],
+  [  0, 60,100],
+  [  0,  0, 90],
+  [  0,  0,110],
+  [  0, 80,100],
+  [  0,  0,230],
+  [119, 11, 32],
+  [  0,  0,142]
+]
+
+def convert_segmentation_to_colour(segmentation_map):
+  width, height = segmentation_map.shape[0:2]
+  colour_segmentation_map = segmentation_map.copy()
+
+  for w in range(width):
+    for h in range(height):
+      colour_segmentation_map[w][h] = cityscapes_colours[colour_segmentation_map[w][h][0]]
+
+  return colour_segmentation_map
+
+
 print('Generate Segmentation ...')
 
 if len(sys.argv) != 4:
@@ -29,6 +80,8 @@ image = Image.open(input_image_path)
 input = torchvision.transforms.functional.to_tensor(image).to(device).unsqueeze(0)
 prediction = model(input)
 
-output = prediction['out']
+output = prediction['out'].argmax(0).cpu().numpy()
+segmentation = convert_segmentation_to_colour(output)
 
 torch.save(output, f'{save_path}/raw_output.pth')
+torch.save(segmentation, f'{save_path}/segmentation.bmp')

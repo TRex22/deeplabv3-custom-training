98f8c2e386667aa998a718a933bbb9886d316db1
trex22 <contact@jasonchalom.com>
2022-05-01 22:35:53 +0200

    Fix missing param and add console output :cop:

--- tools/validate_models.py
+++ tools/validate_models.py
@@ -19,26 +19,28 @@ print('Test pretrained models ...')
 category_list = custom_utils.fetch_category_list(config)
 dev, _summary_dev = custom_utils.fetch_device()
 loss_func = nn.functional.cross_entropy # TODO: Add in weight
+lr_scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(opt, 'min', patience = 5, min_lr = 0.00001) # TODO: Make configurable
 
 # If arg open model
 if len(sys.argv) > 1:
   model_path = sys.argv[1]
+  print(f'Validating {model_path} ...')
   config, _start_epoch, _model_path = custom_utils.open_config(model_path)
 
   model, opt = custom_utils.initialise_model(device, config, num_classes=len(category_list))
   model, _opt = custom_utils.load(model, opt, device, model_path) # Load model
   model.to(dev)
 
-  custom_utils.validate(model, dev, loss_func, -1, config, category_list=category_list, save=False)
+  custom_utils.validate(model, dev, loss_func, lr_scheduler, -1, config, category_list=category_list, save=False)
 else:
   # ResNet50
   print('Validating deeplabv3_resnet50 ...')
   model = models.segmentation.deeplabv3_resnet50(pretrained=True, num_classes=21)
   model.to(dev)
-  custom_utils.validate(model, dev, loss_func, -1, config, category_list=category_list, save=False)
+  custom_utils.validate(model, dev, loss_func, lr_scheduler, -1, config, category_list=category_list, save=False)
 
   # ResNet101
   print('Validating deeplabv3_resnet101 ...')
   model = models.segmentation.deeplabv3_resnet101(pretrained=True, num_classes=21)
   model.to(dev)
-  custom_utils.validate(model, dev, loss_func, -1, config, category_list=category_list, save=False)
+  custom_utils.validate(model, dev, loss_func, lr_scheduler, -1, config, category_list=category_list, save=False)

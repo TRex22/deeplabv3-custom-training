69cbba4f90521da984e8ddd9d65ace3c8f24a797
trex22 <contact@jasonchalom.com>
2022-05-15 01:25:55 +0200

    Get model folder loading working :cop:

--- tools/custom_validation.py
+++ tools/custom_validation.py
@@ -26,7 +26,14 @@ torch.autograd.profiler.emit_nvtx(False)
 ################################################################################
 # Validate Strategies                                                          #
 ################################################################################
-def validate(model_path, model, dev, loss_func, config, category_list, save_path=False):
+def validate(model_path, dev, loss_func, save_path=False):
+  custom_utils.clear_gpu() # Needed to ensure no memory loss
+
+  config, _start_epoch, model_path = custom_utils.open_config(model_path)
+  category_list = custom_utils.fetch_category_list(config)
+
+  model, _opt = custom_utils.initialise_model(dev, config, num_classes=len(category_list), randomise=False)
+
   model, _opt, epoch = custom_utils.load(model, dev, model_path, show_stats=False)
   final_loss, final_dice, final_iou1, final_iou2, final_iou3 = custom_utils.validate(model, dev, loss_func, None, epoch, config, category_list=category_list, save=False)
 
@@ -47,23 +54,19 @@ if __name__ == '__main__':
   model_path = sys.argv[1]
   print(f'Validate: {model_path}')
 
-  config, _start_epoch, model_path = custom_utils.open_config(model_path)
-
   # Based on reference code
   loss_func = nn.functional.cross_entropy # TODO: Add in weight
-  category_list = custom_utils.fetch_category_list(config)
-
-  custom_utils.clear_gpu() # Needed to ensure no memory loss
-
-  model, _opt = custom_utils.initialise_model(dev, config, num_classes=len(category_list), randomise=False)
 
   if len(sys.argv) == 2:
-    validate(model_path, model, dev, loss_func, config, category_list)
+    validate(model_path, dev, loss_func)
   elif len(sys.argv) == 3:
     save_path = sys.argv[2]
-    models = os.listdir(f'{model_path}/*.pth')
+    models = os.listdir(f'{model_path}/')
 
-    for specific_model_path in tqdm.tqdm(models):
-      validate(specific_model_path, model, dev, loss_func, config, category_list, save_path=save_path)
+    for specific_model_path_name in tqdm.tqdm(models):
+      _name, ext = os.path.splitext(specific_model_path_name)
+
+      if ext == '.pth':
+        validate(f'{model_path}/{specific_model_path_name}', dev, loss_func, save_path=save_path)
 
   print("Complete!")
